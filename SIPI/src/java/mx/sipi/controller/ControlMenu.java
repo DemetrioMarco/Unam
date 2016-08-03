75b7ea3783ecaaa9d70b75a21a25eba58a71d1cb
Demetrio <Demetrio@192.168.15.28>
2016-08-03 11:04:40 -0500

    Cambios por inicio

--- SIPI/src/java/mx/sipi/controller/ControlMenu.java
+++ SIPI/src/java/mx/sipi/controller/ControlMenu.java
@@ -5,6 +5,7 @@
  */
 package mx.sipi.controller;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -15,5 +16,60 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean (name = "Ctrl")
 @SessionScoped
 public class ControlMenu {
+    
+    private boolean btnBancosAltas;
+    private boolean btnBancosBajas;
+    private boolean btnBancosCambios;
+    
+    @PostConstruct
+    public void init(){
+        btnBancosAltas = false;
+        btnBancosBajas = false;
+        btnBancosCambios = false;
+    }
+    
+    public void altasBancos(){
+        btnBancosAltas = true;
+        btnBancosBajas = false;
+        btnBancosCambios = false;
+    }
+    
+    public void bajasBancos(){
+        btnBancosAltas = false;
+        btnBancosBajas = true;
+        btnBancosCambios = false;
+    }
+    
+    public void cambiosBancos(){
+        btnBancosAltas = false;
+        btnBancosBajas = false;
+        btnBancosCambios = true;
+    }
+
+    public boolean isBtnBancosAltas() {
+        return btnBancosAltas;
+    }
+
+    public void setBtnBancosAltas(boolean btnBancosAltas) {
+        this.btnBancosAltas = btnBancosAltas;
+    }
+
+    public boolean isBtnBancosBajas() {
+        return btnBancosBajas;
+    }
+
+    public void setBtnBancosBajas(boolean btnBancosBajas) {
+        this.btnBancosBajas = btnBancosBajas;
+    }
+
+    public boolean isBtnBancosCambios() {
+        return btnBancosCambios;
+    }
+
+    public void setBtnBancosCambios(boolean btnBancosCambios) {
+        this.btnBancosCambios = btnBancosCambios;
+    }
+    
+    
 
 }
